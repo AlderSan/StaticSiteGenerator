e5ff79575999a810fd9715722b2d0d8df60cef9f
AlderSan <gentr023@gmail.com>
2026-01-15 14:17:43 -0600

    Implement ParentNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,7 +36,7 @@ class LeafNode(HTMLNode):
 
     def to_html(self) -> str:
         if self.value is None:
-            raise ValueError("node has no value")
+            raise ValueError("leaf node has no value")
         if self.tag is None:
             return self.value
         else:
@@ -51,3 +51,23 @@ class LeafNode(HTMLNode):
             "props": self.props,
         }
         return str(results)    
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str | None, children: list[HTMLNode] | None, props: dict[str, str] | None = None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        html_string = ''
+        open_tag = f'<{self.tag}{self.props_to_html()}>'
+        close_tag = f'</{self.tag}>'
+        if self.tag is None:
+            raise ValueError("parent node has no tag")
+        if self.children is None:
+            raise ValueError("Parent node has no children")
+        else:
+            for child in self.children:
+                if child.value is None and type(child) == LeafNode:
+                    raise ValueError(f'Child node {child} is missing a value')
+                html_string += child.to_html()
+        html_string = open_tag + html_string + close_tag
+        return html_string
