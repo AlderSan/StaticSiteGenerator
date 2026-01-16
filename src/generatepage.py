b154fe0cfeda91860cd6c17ce345abcc594abc70
AlderSan <gentr023@gmail.com>
2026-01-16 13:03:28 -0600

    Implement recursive page generation

--- src/generatepage.py
+++ src/generatepage.py
@@ -30,3 +30,36 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         print(f"An error occured: {e}")
 
     return
+
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    try:
+        cwd = os.getcwd()
+        abs_from = os.path.join(cwd, dir_path_content)
+        abs_dest = os.path.join(cwd, dest_dir_path)
+        abs_template = os.path.join(cwd, template_path)
+        if not os.path.exists(abs_from):
+            raise FileNotFoundError
+        content_list = os.listdir(abs_from)
+        file_list = []
+        dir_list = []
+        for content in content_list:
+            content_path = os.path.join(abs_from, content)
+            if os.path.isfile(content_path):
+                file_list.append(content)
+            else: 
+                dir_list.append(content)
+        for file in file_list:
+            dest_file = os.path.join(dest_dir_path, file)
+            file_path = os.path.join(dir_path_content, file)
+            generate_page(file_path, abs_template, dest_file)
+        for dir in dir_list:
+            new_src = os.path.join(abs_from, dir)
+            new_dest = os.path.join(abs_dest, dir)
+            if not os.path.exists(new_dest):
+                os.mkdir(new_dest)
+            generate_pages_recursive(new_src, template_path, new_dest)
+    except FileNotFoundError:
+        print("source directory does not exist")
+    except Exception as e:
+        print(f"An error has occured: {e}")
+    return
--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import shutil
 from textnode import TextNode, TextType
 from htmlnode import LeafNode, HTMLNode
 from copycontent import copy_content
-from generatepage import generate_page
+from generatepage import generate_page, generate_pages_recursive
 
 
 def main():
     copy_content("static", "public")
-    generate_page("content/index.md", "template.html","public/index.html")
+    generate_pages_recursive("content/", "template.html","public/")
 
 
 if __name__ == "__main__":
